2fc43b996cee7c4cd1d36b3d249ac7e455def9d6
subugee <sasubramanian_md@hotmail.com>
2025-02-21 12:45:11 +0530

    graphql integration

---
+++ graphql-nextclient/src/app/countries/page.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client"
+import { getClient } from "../lib/client"
+
+//Declare Query
+const query = gql`query {
+   countries {
+        name
+    }
+  }
+`
+
+export default async function CountriesPage() {
+    const { data: { countries } } = await getClient().query({ query })
+
+    return <div>
+        <h1>Countries Info From Graphql Server</h1>
+        <ul>
+            {countries.map(country => {
+                return <li>
+                    <h3>{country.name}</h3>
+                </li>
+            })}
+        </ul>
+    </div>
+}
